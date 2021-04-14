0168ea83a4d9b3b7c7564f3fa2b7c5649f341758
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-04-14 18:06:31 -0400

    Sending the ReserveProductCommand to a Command Bus

--- src/main/java/com/appsdeveloperblog/estore/OrdersService/saga/OrderSaga.java
+++ src/main/java/com/appsdeveloperblog/estore/OrdersService/saga/OrderSaga.java
@@ -1,5 +1,8 @@
 package com.appsdeveloperblog.estore.OrdersService.saga;
 
+import org.axonframework.commandhandling.CommandCallback;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.commandhandling.CommandResultMessage;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.StartSaga;
@@ -7,6 +10,7 @@ import org.axonframework.spring.stereotype.Saga;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.appsdeveloperblog.estore.OrdersService.core.events.OrderCreatedEvent;
+import com.appsdeveloperblog.estore.core.commands.ReserveProductCommand;
 
 @Saga
 public class OrderSaga {
@@ -18,6 +22,27 @@ public class OrderSaga {
 	@SagaEventHandler(associationProperty="orderId")
 	public void handle(OrderCreatedEvent orderCreatedEvent) {
 		
+		ReserveProductCommand reserveProductCommand = ReserveProductCommand.builder()
+				.orderId(orderCreatedEvent.getOrderId())
+				.productId(orderCreatedEvent.getProductId())
+				.quantity(orderCreatedEvent.getQuantity())
+				.userId(orderCreatedEvent.getUserId())
+				.build();
+		
+		commandGateway.send(reserveProductCommand, new CommandCallback<ReserveProductCommand, Object>() {
+
+			@Override
+			public void onResult(CommandMessage<? extends ReserveProductCommand> commandMessage,
+					CommandResultMessage<? extends Object> commandResultMessage) {
+				   if(commandResultMessage.isExceptional()) {
+					   // Start a compensating transaction
+				   }
+				
+			}
+			
+		});
+		
+ 
 	}
 
 }
