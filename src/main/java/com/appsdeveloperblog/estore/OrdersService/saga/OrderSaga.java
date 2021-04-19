b5f7963b3dc8d153658c137f3f40feabc82e724e
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-04-19 12:06:51 -0400

    Fetch Payment Details and Publish the ProcessPaymentCommand

--- src/main/java/com/appsdeveloperblog/estore/OrdersService/saga/OrderSaga.java
+++ src/main/java/com/appsdeveloperblog/estore/OrdersService/saga/OrderSaga.java
@@ -1,25 +1,36 @@
 package com.appsdeveloperblog.estore.OrdersService.saga;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 import org.axonframework.commandhandling.CommandCallback;
 import org.axonframework.commandhandling.CommandMessage;
 import org.axonframework.commandhandling.CommandResultMessage;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.StartSaga;
+import org.axonframework.queryhandling.QueryGateway;
 import org.axonframework.spring.stereotype.Saga;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.appsdeveloperblog.estore.OrdersService.core.events.OrderCreatedEvent;
+import com.appsdeveloperblog.estore.core.commands.ProcessPaymentCommand;
 import com.appsdeveloperblog.estore.core.commands.ReserveProductCommand;
 import com.appsdeveloperblog.estore.core.events.ProductReservedEvent;
+import com.appsdeveloperblog.estore.core.model.User;
+import com.appsdeveloperblog.estore.core.query.FetchUserPaymentDetailsQuery;
 
 @Saga
 public class OrderSaga {
 	
 	@Autowired
 	private transient CommandGateway commandGateway;
+	
+	@Autowired
+	private transient QueryGateway queryGateway;
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(OrderSaga.class);
 	
@@ -58,6 +69,49 @@ public class OrderSaga {
 		// Process user payment
         LOGGER.info("ProductReserveddEvent is called for productId: "+ productReservedEvent.getProductId() + 
         		" and orderId: " + productReservedEvent.getOrderId());
+        
+        FetchUserPaymentDetailsQuery fetchUserPaymentDetailsQuery = 
+        		new FetchUserPaymentDetailsQuery(productReservedEvent.getUserId());
+        
+        User userPaymentDetails = null;
+        
+        try {
+        	userPaymentDetails = queryGateway.query(fetchUserPaymentDetailsQuery, ResponseTypes.instanceOf(User.class)).join();
+        } catch(Exception ex) {
+        	LOGGER.error(ex.getMessage());
+        	
+        	// Start compensating transaction
+        	return;
+        }
+        
+        if(userPaymentDetails == null) {
+        	// Start compensating transaction
+        	return;
+        }
+        
+        LOGGER.info("Successfully fetched user payment details for user " + userPaymentDetails.getFirstName());
+        
+        
+        ProcessPaymentCommand proccessPaymentCommand = ProcessPaymentCommand.builder()
+        		.orderId(productReservedEvent.getOrderId())
+        		.paymentDetails(userPaymentDetails.getPaymentDetails())
+        		.paymentId(UUID.randomUUID().toString())
+        		.build();
+        
+        String result = null;
+        try {
+        result = commandGateway.sendAndWait(proccessPaymentCommand, 10, TimeUnit.SECONDS);
+        } catch(Exception ex) {
+        	LOGGER.error(ex.getMessage());
+        	// Start compensating transaction
+        }
+        
+        if(result == null) {
+        	LOGGER.info("The ProcessPaymentCommand resulted in NULL. Initiating a compensating transaction");
+        	// Start compensating transaction
+        }
+        
+        
 	}
 
 }
