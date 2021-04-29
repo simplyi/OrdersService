1e44c5aec3f654427524fc93e3de7d050b763ab9
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-04-29 10:56:24 -0400

    Created a SimpleDeadlineManager Bean

--- src/main/java/com/appsdeveloperblog/estore/OrdersService/OrdersServiceApplication.java
+++ src/main/java/com/appsdeveloperblog/estore/OrdersService/OrdersServiceApplication.java
@@ -1,8 +1,14 @@
 package com.appsdeveloperblog.estore.OrdersService;
 
+import org.axonframework.config.Configuration;
+import org.axonframework.config.ConfigurationScopeAwareProvider;
+import org.axonframework.deadline.DeadlineManager;
+import org.axonframework.deadline.SimpleDeadlineManager;
+import org.axonframework.spring.messaging.unitofwork.SpringTransactionManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,6 +16,16 @@ public class OrdersServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrdersServiceApplication.class, args);
+	}
+	
+	@Bean
+	public DeadlineManager deadlineManager(Configuration configuration, 
+			SpringTransactionManager transactionManager) {
+	
+		return SimpleDeadlineManager.builder()
+				.scopeAwareProvider(new ConfigurationScopeAwareProvider(configuration))
+                .transactionManager(transactionManager)
+                .build();
 	}
 
 }
