08ed93f1d8e7bed4f49f00ad5b5d07f2594e3893
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-05-04 14:08:56 -0400

    Handle the FindOrderQuery

---
+++ src/main/java/com/appsdeveloperblog/estore/OrdersService/core/model/OrderSummary.java
@@ -0,0 +1,11 @@
+package com.appsdeveloperblog.estore.OrdersService.core.model;
+
+import lombok.Value;
+
+@Value
+public class OrderSummary {
+
+	private final String orderId;
+	private final OrderStatus orderStatus;
+	
+}
---
+++ src/main/java/com/appsdeveloperblog/estore/OrdersService/query/OrderQueriesHandler.java
@@ -0,0 +1,25 @@
+package com.appsdeveloperblog.estore.OrdersService.query;
+
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.stereotype.Component;
+
+import com.appsdeveloperblog.estore.OrdersService.core.data.OrderEntity;
+import com.appsdeveloperblog.estore.OrdersService.core.data.OrdersRepository;
+import com.appsdeveloperblog.estore.OrdersService.core.model.OrderSummary;
+
+@Component
+public class OrderQueriesHandler {
+
+	OrdersRepository ordersRepository;
+
+	public OrderQueriesHandler(OrdersRepository ordersRepository) {
+		this.ordersRepository = ordersRepository;
+	}
+
+	@QueryHandler
+	public OrderSummary findOrder(FindOrderQuery findOrderQuery) {
+		OrderEntity orderEntity = ordersRepository.findByOrderId(findOrderQuery.getOrderId());
+		return new OrderSummary(orderEntity.getOrderId(), orderEntity.getOrderStatus());
+	}
+
+}
