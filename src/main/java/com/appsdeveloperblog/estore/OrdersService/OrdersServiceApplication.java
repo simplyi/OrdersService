528675b037b96b3d394df2b84095c9c76f36ea3f
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-05-02 13:50:32 -0400

    Fixed issue with XStream ForbiddenClassException

--- src/main/java/com/appsdeveloperblog/estore/OrdersService/OrdersServiceApplication.java
+++ src/main/java/com/appsdeveloperblog/estore/OrdersService/OrdersServiceApplication.java
@@ -9,9 +9,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+import com.appsdeveloperblog.estore.core.config.XStreamConfig;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@Import({ XStreamConfig.class })
 public class OrdersServiceApplication {
 
 	public static void main(String[] args) {
